c1eff91240c1725415deed87d361273b0ac383b2
urielpereiraalves <upereiraalves@hotmail.com>
2022-04-06 15:20:30 -0300

    exercicios cshsarp

---
+++ taboada 1/aula6 visualstudio/aula6 visualstudio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace aula6_visualstudio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 0;
+
+            for (int i = 1; i <= 20; i++) ;
+            {
+                Console.WriteLine($" posição {i} valor que deseija somar {valor}:");
+                valor += int.Parse(Console.ReadLine());
+            }
+           Console.WriteLine($"somatótia: {valor}");
+                
+        }
+    }
+}
---
+++ taboada2/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("tabuada!");
+            Console.WriteLine("insira o numero que voce quer multiplicar:");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int x = 1; x <= 10; x++)
+            {
+                int resultado = numero * x;
+                Console.WriteLine($"{numero} X {x} = {resultado}");
+
+            }
+        }
+        
+        
+    }
+}
