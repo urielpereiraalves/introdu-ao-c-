6116849e9ba57a6828551102e4176e86f0317be0
urielpereiraalves <upereiraalves@hotmail.com>
2022-04-08 23:40:05 -0300

    exercicio 8 csharp

---
+++ aula 08 visu/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace aula_08_visu src
+{
+   public class Gato
+    {
+        public String Nome { get; set; }
+        public String Cor { get; set; }
+        public int Tamanho { get; set; }
+
+    public Gato()
+        {
+            
+        }
+    public Gato(String = Nome,String = Cor,int = Tamanho)
+    }
+    Nome = nome
+    Cor = cor
+    Tamanho = _tamanho;
+}
+{
+    public void dormir ()
+}
+{
+    Console.WriteLine('zzzz');
+}
+public void comer(String comida)
+{
+    Console.WriteLine
+}
